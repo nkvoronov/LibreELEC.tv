9fa55aae4bf500a2631a78b76377adff165de25d
Nikolay Voronov <nkvoronov@gmail.com>
2016-08-12 15:20:20 +0300

    fix

--- build_addons-Generic_x86_64.sh
+++ build_addons-Generic_x86_64.sh
@@ -10,5 +10,4 @@ PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mupen64plus-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent-addon
-
-
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon elec-modify-addon
