503a4ba5dc2480a720429db6455a3f067aab239d
Nikolay Voronov <nkvoronov@gmail.com>
2016-09-27 03:34:21 +0300

    update

--- packages/custom/mediacentre/kodi-theme-confluence-extented-s/package.mk
+++ packages/custom/mediacentre/kodi-theme-confluence-extented-s/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-confluence-extented-s"
-PKG_VERSION="4617eb5"
+PKG_VERSION="1d074f6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
--- packages/custom/mediacentre/kodi-theme-confluence-extented/package.mk
+++ packages/custom/mediacentre/kodi-theme-confluence-extented/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-confluence-extented"
-PKG_VERSION="f77eeae"
+PKG_VERSION="0f5b89a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
