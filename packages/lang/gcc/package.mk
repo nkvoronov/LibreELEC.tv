20a35216c3e722ab92d62e7cb3b037b5d2734c5f
Nikolay Voronov <nkvoronov@gmail.com>
2016-10-13 21:17:34 +0300

    fixes5

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -84,7 +84,7 @@ PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
                          --disable-libstdcxx-pch \
                          --enable-libstdcxx-time \
                          --enable-clocale=gnu \
-                         --disable-libatomic \
+                         --enable-libatomic \
                          $GCC_OPTS"
 
 pre_configure_host() {
