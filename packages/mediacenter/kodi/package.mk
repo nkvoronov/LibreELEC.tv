534b24d9f83a1321d97e2a3b8c8543326bf72d77
Nikolay Voronov <nkvoronov@gmail.com>
2016-11-12 21:50:02 +0200

    update Kodi 2

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="a458002"
+PKG_VERSION="ae37dd6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
