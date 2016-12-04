347da4d5c3ddac70ec8343077bc5b59aedd7b07d
Nikolay Voronov <nkvoronov@gmail.com>
2016-12-04 04:18:24 +0200

    update

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="1f9756d"
+PKG_VERSION="1dd8613"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
