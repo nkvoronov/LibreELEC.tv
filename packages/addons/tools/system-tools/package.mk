8843092dc5243bce647fb99c4796c2e659e0c5d1
Nikolay Voronov <nkvoronov@gmail.com>
2016-12-26 20:38:08 +0200

    fixes6

--- packages/addons/tools/system-tools/package.mk
+++ packages/addons/tools/system-tools/package.mk
@@ -32,7 +32,6 @@ PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="System Tools"
 PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_PROVIDES=""
-PKG_ADDON_REPOVERSION="7.0"
 
 PKG_AUTORECONF="no"
 
@@ -43,14 +42,17 @@ ENABLE_EFIBOOTMGR="no"
 ENABLE_EVTEST="yes"
 ENABLE_FDUPES="no"
 ENABLE_FILE="no"
-ENABLE_GETSCANCODES="no" 
+ENABLE_GETSCANCODES="no"
 ENABLE_HDDTEMP="yes"
 ENABLE_HD-IDLE="no"
 ENABLE_HID_MAPPER="no"
 ENABLE_I2C-TOOLS="no"
+ENABLE_INOTIFY-TOOLS="no"
 ENABLE_JQ="no"
 ENABLE_LM_SENSORS="yes"
 ENABLE_LSHW="no"
+ENABLE_MC="yes"
+ENABLE_HTOP="yes"
 ENABLE_MRXVT="no"
 ENABLE_MTPFS="no"
 ENABLE_NMON="no"
@@ -61,8 +63,6 @@ ENABLE_SCREEN="no"
 ENABLE_STRACE="no"
 ENABLE_UNRAR="yes"
 ENABLE_USB-MODESWITCH="no"
-ENABLE_MC="yes"
-ENABLE_HTOP="yes"
 
 if [ "$ENABLE_AUTOSSH" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET autossh"
@@ -112,6 +112,10 @@ if [ "$ENABLE_I2C-TOOLS" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET i2c-tools"
 fi
 
+if [ "$ENABLE_INOTIFY-TOOLS" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inotify-tools"
+fi
+
 if [ "$ENABLE_JQ" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET jq"
 fi
@@ -122,6 +126,14 @@ fi
 
 if [ "$ENABLE_LSHW" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET lshw"
+fi
+
+if [ "$ENABLE_MC" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc"
+fi
+
+if [ "$ENABLE_HTOP" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET htop"
 fi
 
 if [ "$ENABLE_MRXVT" = yes ]; then
@@ -164,67 +176,60 @@ if [ "$ENABLE_USB-MODESWITCH" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET usb-modeswitch"
 fi
 
-if [ "$ENABLE_MC" = yes ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc"
-fi
-
-if [ "$ENABLE_HTOP" = yes ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET htop"
-fi
-
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib/
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/data/
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin/
-
+    # autossh
   if [ "$ENABLE_AUTOSSH" = yes ]; then
     cp -P $(get_build_dir autossh)/.$TARGET_NAME/autossh $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # diffutils
   if [ "$ENABLE_DIFFUTILS" = yes ]; then
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/cmp $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/diff $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/diff3 $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/sdiff $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # dtach
   if [ "$ENABLE_DTACH" = yes ]; then
     cp -P $(get_build_dir dtach)/.$TARGET_NAME/dtach $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # efibootmgr
   if [ "$ENABLE_EFIBOOTMGR" = yes ]; then
     cp -P $(get_build_dir efibootmgr)/src/efibootmgr/efibootmgr $ADDON_BUILD/$PKG_ADDON_ID/bin 2>/dev/null || :
   fi
-
+    # evtest
   if [ "$ENABLE_EVTEST" = yes ]; then
     cp -P $(get_build_dir evtest)/.$TARGET_NAME/evtest $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # fdupes
   if [ "$ENABLE_FDUPES" = yes ]; then
     cp -P $(get_build_dir fdupes)/fdupes $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # file
   if [ "$ENABLE_FILE" = yes ]; then
     cp -P $(get_build_dir file)/.$TARGET_NAME/src/file $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir file)/.$TARGET_NAME/magic/magic.mgc $ADDON_BUILD/$PKG_ADDON_ID/data
   fi
-
+    # getscancodes
   if [ "$ENABLE_GETSCANCODES" = yes ]; then
     cp -P $(get_build_dir getscancodes)/getscancodes $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # hddtemp
   if [ "$ENABLE_HDDTEMP" = yes ]; then
     cp -P $(get_build_dir hddtemp)/.$TARGET_NAME/src/hddtemp $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_build_dir hddtemp)/debian/hddtemp.db $ADDON_BUILD/$PKG_ADDON_ID/data
   fi
-
+    # hd-idle
   if [ "$ENABLE_HD-IDLE" = yes ]; then
     cp -P $(get_build_dir hd-idle)/hd-idle $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # hid_mapper
   if [ "$ENABLE_HID_MAPPER" = yes ]; then
     cp -P $(get_build_dir hid_mapper)/hid_mapper $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # i2c-tools
   if [ "$ENABLE_I2C-TOOLS" = yes ]; then
     cp -P $(get_build_dir i2c-tools)/tools/i2cdetect $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir i2c-tools)/tools/i2cdump $ADDON_BUILD/$PKG_ADDON_ID/bin
@@ -232,68 +237,74 @@ addon() {
     cp -P $(get_build_dir i2c-tools)/tools/i2cset $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir i2c-tools)/py-smbus/build/lib.linux-*/smbus.so $ADDON_BUILD/$PKG_ADDON_ID/lib
   fi
-
+    # inotify-tools
+  if [ "$ENABLE_INOTIFY_TOOLS" = yes ]; then
+    cp -P $(get_build_dir inotify-tools)/.$TARGET_NAME/src/inotifywait $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_build_dir inotify-tools)/.$TARGET_NAME/src/inotifywatch $ADDON_BUILD/$PKG_ADDON_ID/bin
+  fi
+    # jq
   if [ "$ENABLE_JQ" = yes ]; then
     cp -P $(get_build_dir jq)/.$TARGET_NAME/jq $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # lm_sensors
   if [ "$ENABLE_LM_SENSORS" = yes ]; then
     cp -P $(get_build_dir lm_sensors)/prog/sensors/sensors $ADDON_BUILD/$PKG_ADDON_ID/bin 2>/dev/null || :
   fi
-
+    # lshw
   if [ "$ENABLE_LSHW" = yes ]; then
     cp -P $(get_build_dir lshw)/src/lshw $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # mc
+  if [ "$ENABLE_MC" = yes ]; then
+    cp -Pa $(get_build_dir mc)/.install_pkg/usr/bin/* $ADDON_BUILD/$PKG_ADDON_ID/bin/
+    cp -Pa $(get_build_dir mc)/.install_pkg/storage/.kodi/addons/virtual.system-tools/* $ADDON_BUILD/$PKG_ADDON_ID
+  fi
+    # htop
+  if [ "$ENABLE_HTOP" = yes ]; then
+    cp -P $(get_build_dir htop)/htop $ADDON_BUILD/$PKG_ADDON_ID/bin
+  fi
+    # mrxvt
   if [ "$ENABLE_MRXVT" = yes ]; then
     cp -P $(get_build_dir mrxvt)/.$TARGET_NAME/src/mrxvt $ADDON_BUILD/$PKG_ADDON_ID/bin 2>/dev/null || :
   fi
-
+    # mtpfs
   if [ "$ENABLE_MTPFS" = yes ]; then
     cp -P $(get_build_dir mtpfs)/.$TARGET_NAME/mtpfs $ADDON_BUILD/$PKG_ADDON_ID/bin/
   fi
-
+    # nmon
   if [ "$ENABLE_NMON" = yes ]; then
     cp -P $(get_build_dir nmon)/nmon $ADDON_BUILD/$PKG_ADDON_ID/bin/
   fi
-
+    # p7zip
   if [ "$ENABLE_P7ZIP" = yes ]; then
     cp -P $(get_build_dir p7zip)/bin/7z.so $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -PR $(get_build_dir p7zip)/bin/Codecs $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir p7zip)/bin/7z $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir p7zip)/bin/7za $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # patch
   if [ "$ENABLE_PATCH" = yes ]; then
     cp -P $(get_build_dir patch)/.$TARGET_NAME/src/patch $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # pv
   if [ "$ENABLE_PV" = yes ]; then
     cp -P $(get_build_dir pv)/.$TARGET_NAME/pv $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # screen
   if [ "$ENABLE_SCREEN" = yes ]; then
     cp -P $(get_build_dir screen)/screen $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # strace
   if [ "$ENABLE_STRACE" = yes ]; then
     cp -P $(get_build_dir strace)/.$TARGET_NAME/strace $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # unrar
   if [ "$ENABLE_UNRAR" = yes ]; then
     cp -P $(get_build_dir unrar)/unrar $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
-
+    # usb-modeswitch
   if [ "$ENABLE_USB-MODESWITCH" = yes ]; then
     cp -P $(get_build_dir usb-modeswitch)/usb_modeswitch $ADDON_BUILD/$PKG_ADDON_ID/bin
-  fi
-
-  if [ "$ENABLE_MC" = yes ]; then
-    cp -P $(get_build_dir mc)/.install_pkg/usr/bin/mc $ADDON_BUILD/$PKG_ADDON_ID/bin
-  fi
-
-  if [ "$ENABLE_HTOP" = yes ]; then
-    cp -P $(get_build_dir htop)/htop $ADDON_BUILD/$PKG_ADDON_ID/bin
   fi
 
   debug_strip $ADDON_BUILD/$PKG_ADDON_ID/bin
--- packages/custom/tools/tools-system/package.mk
+++ packages/custom/tools/tools-system/package.mk
@@ -40,12 +40,15 @@ ENABLE_FDUPES="no"
 ENABLE_FILE="no"
 ENABLE_GETSCANCODES="no"
 ENABLE_HDDTEMP="yes"
-ENABLE_HD_IDLE="no"
+ENABLE_HD-IDLE="no"
 ENABLE_HID_MAPPER="no"
-ENABLE_I2C_TOOLS="no"
+ENABLE_I2C-TOOLS="no"
+ENABLE_INOTIFY-TOOLS="no"
 ENABLE_JQ="no"
 ENABLE_LM_SENSORS="yes"
 ENABLE_LSHW="no"
+ENABLE_MC="yes"
+ENABLE_HTOP="yes"
 ENABLE_MRXVT="no"
 ENABLE_MTPFS="no"
 ENABLE_NMON="no"
@@ -55,9 +58,7 @@ ENABLE_PV="no"
 ENABLE_SCREEN="no"
 ENABLE_STRACE="no"
 ENABLE_UNRAR="yes"
-ENABLE_USB_MODESWITCH="no"
-ENABLE_MC="yes"
-ENABLE_HTOP="yes"
+ENABLE_USB-MODESWITCH="no"
 
 if [ "$ENABLE_AUTOSSH" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET autossh"
@@ -95,7 +96,7 @@ if [ "$ENABLE_HDDTEMP" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET hddtemp"
 fi
 
-if [ "$ENABLE_HD_IDLE" = yes ]; then
+if [ "$ENABLE_HD-IDLE" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET hd-idle"
 fi
 
@@ -103,8 +104,12 @@ if [ "$ENABLE_HID_MAPPER" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET hid_mapper"
 fi
 
-if [ "$ENABLE_I2C_TOOLS" = yes ]; then
+if [ "$ENABLE_I2C-TOOLS" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET i2c-tools"
+fi
+
+if [ "$ENABLE_INOTIFY-TOOLS" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inotify-tools"
 fi
 
 if [ "$ENABLE_JQ" = yes ]; then
@@ -117,6 +122,14 @@ fi
 
 if [ "$ENABLE_LSHW" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET lshw"
+fi
+
+if [ "$ENABLE_MC" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc"
+fi
+
+if [ "$ENABLE_HTOP" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET htop"
 fi
 
 if [ "$ENABLE_MRXVT" = yes ]; then
@@ -155,16 +168,8 @@ if [ "$ENABLE_UNRAR" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET unrar"
 fi
 
-if [ "$ENABLE_USB_MODESWITCH" = yes ]; then
+if [ "$ENABLE_USB-MODESWITCH" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET usb-modeswitch"
-fi
-
-if [ "$ENABLE_MC" = yes ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc"
-fi
-
-if [ "$ENABLE_HTOP" = yes ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET htop"
 fi
 
 make_target() {
@@ -176,123 +181,86 @@ makeinstall_target() {
 }
 
 post_install() {
-
   mkdir -p $INSTALL/
   mkdir -p $INSTALL/usr/bin/
   mkdir -p $INSTALL/usr/lib/
-
+    # autossh
   if [ "$ENABLE_AUTOSSH" = yes ]; then
     cp -P $(get_build_dir autossh)/.$TARGET_NAME/autossh $INSTALL/usr/bin
   fi
-
+    # diffutils
   if [ "$ENABLE_DIFFUTILS" = yes ]; then
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/cmp $INSTALL/usr/bin
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/diff $INSTALL/usr/bin
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/diff3 $INSTALL/usr/bin
     cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/sdiff $INSTALL/usr/bin
   fi
-
+    # dtach
   if [ "$ENABLE_DTACH" = yes ]; then
     cp -P $(get_build_dir dtach)/.$TARGET_NAME/dtach $INSTALL/usr/bin
   fi
-
+    # efibootmgr
   if [ "$ENABLE_EFIBOOTMGR" = yes ]; then
     cp -P $(get_build_dir efibootmgr)/src/efibootmgr/efibootmgr $INSTALL/usr/bin 2>/dev/null || :
   fi
-
+    # evtest
   if [ "$ENABLE_EVTEST" = yes ]; then
     cp -P $(get_build_dir evtest)/.$TARGET_NAME/evtest $INSTALL/usr/bin
   fi
-
+    # fdupes
   if [ "$ENABLE_FDUPES" = yes ]; then
     cp -P $(get_build_dir fdupes)/fdupes $INSTALL/usr/bin
   fi
-
-  if [ "$ENABLE_FILE" = yes ]; then    
+    # file
+  if [ "$ENABLE_FILE" = yes ]; then
     cp -P $(get_build_dir file)/.$TARGET_NAME/src/file $INSTALL/usr/bin
     mkdir -p $INSTALL/usr/data/
     cp -P $(get_build_dir file)/.$TARGET_NAME/magic/magic.mgc $INSTALL/usr/data
   fi
-
+    # getscancodes
   if [ "$ENABLE_GETSCANCODES" = yes ]; then
     cp -P $(get_build_dir getscancodes)/getscancodes $INSTALL/usr/bin
   fi
 
   if [ "$ENABLE_HDDTEMP" = yes ]; then
     cp -P $(get_build_dir hddtemp)/.$TARGET_NAME/src/hddtemp $INSTALL/usr/bin
+    mkdir -p $INSTALL/etc
+    cp -P $(get_build_dir hddtemp)/debian/hddtemp.db $INSTALL/etc
   fi
-
-  if [ "$ENABLE_HD_IDLE" = yes ]; then
+    # hd-idle
+  if [ "$ENABLE_HD-IDLE" = yes ]; then
     cp -P $(get_build_dir hd-idle)/hd-idle $INSTALL/usr/bin
   fi
-
+    # hid_mapper
   if [ "$ENABLE_HID_MAPPER" = yes ]; then
     cp -P $(get_build_dir hid_mapper)/hid_mapper $INSTALL/usr/bin
   fi
-
-  if [ "$ENABLE_I2C_TOOLS" = yes ]; then
+    # i2c-tools
+  if [ "$ENABLE_I2C-TOOLS" = yes ]; then
     cp -P $(get_build_dir i2c-tools)/tools/i2cdetect $INSTALL/usr/bin
     cp -P $(get_build_dir i2c-tools)/tools/i2cdump $INSTALL/usr/bin
     cp -P $(get_build_dir i2c-tools)/tools/i2cget $INSTALL/usr/bin
     cp -P $(get_build_dir i2c-tools)/tools/i2cset $INSTALL/usr/bin
     cp -P $(get_build_dir i2c-tools)/py-smbus/build/lib.linux-*/smbus.so $INSTALL/usr/lib
   fi
-
+    # inotify-tools
+  if [ "$ENABLE_INOTIFY_TOOLS" = yes ]; then
+    cp -P $(get_build_dir inotify-tools)/.$TARGET_NAME/src/inotifywait $INSTALL/usr/bin
+    cp -P $(get_build_dir inotify-tools)/.$TARGET_NAME/src/inotifywatch $INSTALL/usr/bin
+  fi
+    # jq
   if [ "$ENABLE_JQ" = yes ]; then
     cp -P $(get_build_dir jq)/.$TARGET_NAME/jq $INSTALL/usr/bin
   fi
-
+    # lm_sensors
   if [ "$ENABLE_LM_SENSORS" = yes ]; then
     cp -P $(get_build_dir lm_sensors)/prog/sensors/sensors $INSTALL/usr/bin 2>/dev/null || :
   fi
-
+    # lshw
   if [ "$ENABLE_LSHW" = yes ]; then
     cp -P $(get_build_dir lshw)/src/lshw $INSTALL/usr/bin
   fi
-
-  if [ "$ENABLE_MRXVT" = yes ]; then
-    cp -P $(get_build_dir mrxvt)/.$TARGET_NAME/src/mrxvt $INSTALL/usr/bin 2>/dev/null || :
-  fi
-
-  if [ "$ENABLE_MTPFS" = yes ]; then
-    cp -P $(get_build_dir mtpfs)/.$TARGET_NAME/mtpfs $INSTALL/usr/bin/
-  fi
-
-  if [ "$ENABLE_NMON" = yes ]; then
-    cp -P $(get_build_dir nmon)/nmon $INSTALL/usr/bin/
-  fi
-
-  if [ "$ENABLE_P7ZIP" = yes ]; then
-    cp -P $(get_build_dir p7zip)/bin/7z.so $INSTALL/usr/bin
-    cp -PR $(get_build_dir p7zip)/bin/Codecs $INSTALL/usr/bin
-    cp -P $(get_build_dir p7zip)/bin/7z $INSTALL/usr/bin
-    cp -P $(get_build_dir p7zip)/bin/7za $INSTALL/usr/bin
-  fi
-
-  if [ "$ENABLE_PATCH" = yes ]; then
-    cp -P $(get_build_dir patch)/.$TARGET_NAME/src/patch $INSTALL/usr/bin
-  fi
-
-  if [ "$ENABLE_PV" = yes ]; then
-    cp -P $(get_build_dir pv)/.$TARGET_NAME/pv $INSTALL/usr/bin
-  fi
-
-  if [ "$ENABLE_SCREEN" = yes ]; then
-    cp -P $(get_build_dir screen)/screen $INSTALL/usr/bin
-  fi
-
-  if [ "$ENABLE_STRACE" = yes ]; then
-    cp -P $(get_build_dir strace)/.$TARGET_NAME/strace $INSTALL/usr/bin
-  fi
-
-  if [ "$ENABLE_UNRAR" = yes ]; then
-    cp -P $(get_build_dir unrar)/unrar $INSTALL/usr/bin
-  fi
-
-  if [ "$ENABLE_USB_MODESWITCH" = yes ]; then
-    cp -P $(get_build_dir usb-modeswitch)/usb_modeswitch $INSTALL/usr/bin
-  fi
-
+    # mc
   if [ "$ENABLE_MC" = yes ]; then
     cp -PR  $(get_build_dir mc)/.install_pkg/* $INSTALL
 
@@ -309,8 +277,51 @@ post_install() {
       cp -p $fgmo $INSTALL/usr/share/locale/$fname/LC_MESSAGES/mc.mo
     done
   fi
-
+    # htop
   if [ "$ENABLE_HTOP" = yes ]; then
     cp -P $(get_build_dir htop)/htop $INSTALL/usr/bin
+  fi
+    # mrxvt
+  if [ "$ENABLE_MRXVT" = yes ]; then
+    cp -P $(get_build_dir mrxvt)/.$TARGET_NAME/src/mrxvt $INSTALL/usr/bin 2>/dev/null || :
+  fi
+    # mtpfs
+  if [ "$ENABLE_MTPFS" = yes ]; then
+    cp -P $(get_build_dir mtpfs)/.$TARGET_NAME/mtpfs $INSTALL/usr/bin/
+  fi
+    # nmon
+  if [ "$ENABLE_NMON" = yes ]; then
+    cp -P $(get_build_dir nmon)/nmon $INSTALL/usr/bin/
+  fi
+    # p7zip
+  if [ "$ENABLE_P7ZIP" = yes ]; then
+    cp -P $(get_build_dir p7zip)/bin/7z.so $INSTALL/usr/bin
+    cp -PR $(get_build_dir p7zip)/bin/Codecs $INSTALL/usr/bin
+    cp -P $(get_build_dir p7zip)/bin/7z $INSTALL/usr/bin
+    cp -P $(get_build_dir p7zip)/bin/7za $INSTALL/usr/bin
+  fi
+    # patch
+  if [ "$ENABLE_PATCH" = yes ]; then
+    cp -P $(get_build_dir patch)/.$TARGET_NAME/src/patch $INSTALL/usr/bin
+  fi
+    # pv
+  if [ "$ENABLE_PV" = yes ]; then
+    cp -P $(get_build_dir pv)/.$TARGET_NAME/pv $INSTALL/usr/bin
+  fi
+    # screen
+  if [ "$ENABLE_SCREEN" = yes ]; then
+    cp -P $(get_build_dir screen)/screen $INSTALL/usr/bin
+  fi
+    # strace
+  if [ "$ENABLE_STRACE" = yes ]; then
+    cp -P $(get_build_dir strace)/.$TARGET_NAME/strace $INSTALL/usr/bin
+  fi
+    # unrar
+  if [ "$ENABLE_UNRAR" = yes ]; then
+    cp -P $(get_build_dir unrar)/unrar $INSTALL/usr/bin
+  fi
+    # usb-modeswitch
+  if [ "$ENABLE_USB-MODESWITCH" = yes ]; then
+    cp -P $(get_build_dir usb-modeswitch)/usb_modeswitch $INSTALL/usr/bin
   fi
 }
