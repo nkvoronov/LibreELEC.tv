3ed63d84a4f35022d18ea8dbfc9dd205660efe85
Nikolay Voronov <nkvoronov@gmail.com>
2017-01-21 02:02:52 +0200

    fixes

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -265,7 +265,7 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/share/icons
   rm -rf $INSTALL/usr/share/pixmaps
   rm -rf $INSTALL/usr/share/kodi/addons/skin.estouchy
-  #rm -rf $INSTALL/usr/share/kodi/addons/skin.estuary
+  rm -rf $INSTALL/usr/share/kodi/addons/skin.estuary
   rm -rf $INSTALL/usr/share/kodi/addons/service.xbmc.versioncheck
   rm -rf $INSTALL/usr/share/kodi/addons/visualization.vortex
   rm -rf $INSTALL/usr/share/xsessions
