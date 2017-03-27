5156a15f20ced13b27d7a73158e915becc1164d2
Nikolay Voronov <nkvoronov@gmail.com>
2017-03-28 01:31:40 +0300

    fixes

--- packages/custom/graphics/ImageMagick/package.mk
+++ packages/custom/graphics/ImageMagick/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ImageMagick"
-PKG_VERSION="7.0.5-0"
+PKG_VERSION="7.0.5-4"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.imagemagick.org"
