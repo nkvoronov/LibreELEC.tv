a498cb845eda43fad7f4311bc1f1c4d14c89dab3
Nikolay Voronov <nkvoronov@gmail.com>
2017-03-31 07:16:17 +0300

    update

--- packages/addons/service/emby/package.mk
+++ packages/addons/service/emby/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="emby"
-PKG_VERSION="3.2.8"
-PKG_REV="115"
+PKG_VERSION="3.2.10"
+PKG_REV="116"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://emby.media"
