f7eb1a239f4cd534c4eff30e453a68a6c69bf05a
Nikolay Voronov <nkvoronov@gmail.com>
2017-04-13 17:56:12 +0300

    update3

--- packages/mediacenter/kodi-binary-addons/pvr.hts/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.hts/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.hts"
-PKG_VERSION="f70531c"
+PKG_VERSION="cab4353"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
