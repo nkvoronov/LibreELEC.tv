659c4b3de7a76ee10115f784fb0acc8bb29aea06
Nikolay Voronov <nkvoronov@gmail.com>
2017-09-06 03:42:26 +0300

    fix

--- build_addons-Generic_x86_64.sh
+++ build_addons-Generic_x86_64.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon scan-s2-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon szap-s2-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc-addon
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mono
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon emby
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon scan-s2-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon szap-s2-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mono
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon emby
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
