4900a3847a717307419a6e4cf9f0a13a5c392619
Nikolay Voronov <nkvoronov@gmail.com>
2017-09-06 03:41:25 +0300

    tvheadend fix

--- packages/custom/multimedia/tvheadend/package.mk
+++ packages/custom/multimedia/tvheadend/package.mk
@@ -17,14 +17,14 @@
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="3df769a"
-PKG_VERSION_NUMBER="4.3.357"
+PKG_VERSION="3ccebae"
+PKG_VERSION_NUMBER="4.3.425"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
 PKG_URL="https://github.com/tvheadend/tvheadend.git"
 PKG_TYPE="git"
-PKG_DEPENDS_TARGET="toolchain curl libdvbcsa libiconv openssl Python:host yasm"
+PKG_DEPENDS_TARGET="toolchain curl libdvbcsa libiconv openssl pngquant:host Python:host yasm"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="Tvheadend: a TV streaming server for Linux"
 PKG_LONGDESC="Tvheadend ($PKG_VERSION_NUMBER): is a TV streaming server for Linux supporting DVB-S/S2, DVB-C, DVB-T/T2, IPTV, SAT>IP, ATSC and ISDB-T"
@@ -35,35 +35,31 @@ PKG_AUTORECONF="no"
 # transcoding only for generic
 if [ "$TARGET_ARCH" = x86_64 ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libva-intel-driver"
-  TVH_TRANSCODING="--enable-ffmpeg_static --enable-libav --enable-libfdkaac --disable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --disable-qsv"
+  TVH_TRANSCODING="--enable-libav --enable-ffmpeg_static --enable-libx264 --enable-libx265 --enable-libvpx --disable-libtheora --enable-libvorbis --disable-libfdkaac --disable-libopus"
 else
-  TVH_TRANSCODING="--disable-ffmpeg_static --disable-libav"
+  TVH_TRANSCODING="--disable-libav --disable-ffmpeg_static"
 fi
 
 PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
                            --arch=$TARGET_ARCH \
                            --cpu=$TARGET_CPU \
                            --cc=$CC \
+                           --python=$TOOLCHAIN/bin/python \
+                           --enable-hdhomerun_client \
                            --disable-avahi \
-                           --enable-bundle \
-                           --disable-dbus_1 \
+                           $TVH_TRANSCODING \
+                           --disable-uriparser \
                            --enable-dvbcsa \
                            --disable-dvben50221 \
-                           --enable-hdhomerun_client \
-                           --enable-hdhomerun_static \
-                           --enable-epoll \
-                           --enable-inotify \
-                           --disable-nvenc \
-                           --disable-uriparser \
-                           $TVH_TRANSCODING \
                            --enable-tvhcsa \
-                           --enable-trace \
-                           --nowerror \
-                           --disable-bintray_cache \
-                           --python=$TOOLCHAIN/bin/python"
+                           --disable-dbus_1 \
+                           --enable-pngquant \
+                           --enable-bundle \
+                           --nowerror"
 
 post_unpack() {
   sed -e 's/VER="0.0.0~unknown"/VER="'$PKG_VERSION_NUMBER'~g'$PKG_VERSION'~libreelec"/g' -i $PKG_BUILD/support/version
+  sed -e 's|'/usr/bin/pngquant'|'$TOOLCHAIN/bin/pngquant'|g' -i $PKG_BUILD/support/mkbundle
 }
 
 pre_configure_target() {
@@ -74,6 +70,8 @@ pre_configure_target() {
 # transcoding
   if [ "$TARGET_ARCH" = x86_64 ]; then
     export AS=$TOOLCHAIN/bin/yasm
+    export LDFLAGS="$LDFLAGS -lX11 -lm -lvdpau -lva -lva-drm -lva-x11 -lbz2"
+    export ARCH=$TARGET_ARCH
   fi
 
   export CROSS_COMPILE=$TARGET_PREFIX
