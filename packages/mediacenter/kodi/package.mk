28ac632c9ab1dfe9d9b953fd0192563202b2e1bb
Nikolay Voronov <nkvoronov@gmail.com>
2017-09-26 14:57:08 +0300

    upd

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="07424d3"
+PKG_VERSION="3a9c9de"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
