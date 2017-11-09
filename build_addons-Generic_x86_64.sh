e25aadbd1f88b2a818714f403903b7b8feb14e53
Nikolay Voronov <nkvoronov@gmail.com>
2017-11-09 16:49:49 +0200

    upd

--- build_addons-Generic_x86_64.sh
+++ build_addons-Generic_x86_64.sh
@@ -9,6 +9,6 @@ PROJECT=Generic ARCH=x86_64 ./scripts/create_addon szap-s2-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent-addon
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mono
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon emby
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
