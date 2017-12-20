1763e1df21d10084b16f28d74611b6f9600b30fe
Nikolay Voronov <nkvoronov@gmail.com>
2017-12-20 21:32:35 +0200

    update2

--- packages/custom/addons/service/oscam-addon/package.mk
+++ packages/custom/addons/service/oscam-addon/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="oscam-addon"
-PKG_VERSION="11392"
-PKG_REV="122"
+PKG_VERSION="11395"
+PKG_REV="123"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
--- packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.vdr.vnsi"
-PKG_VERSION="fa1e4f5"
+PKG_VERSION="782590c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
