51c0568283a1f4fff6c6539ab86025c08bc4d0d4
Nikolay Voronov <nkvoronov@gmail.com>
2018-03-22 10:58:45 +0200

    update

--- build_addons-Generic_x86_64.sh
+++ build_addons-Generic_x86_64.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon scan-s2-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon szap-s2-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent-addon
-PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc-addon
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr22
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream31
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc3
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mono
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon emby
