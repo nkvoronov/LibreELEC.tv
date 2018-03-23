56047b3c4d77a5c514e2f2cc05001d7fd4dda093
Nikolay Voronov <nkvoronov@gmail.com>
2018-03-23 18:03:30 +0200

    update2

--- build_addons-Generic_x86_64.sh
+++ build_addons-Generic_x86_64.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr22
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream31
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr22
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream31
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc3
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mono
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon emby
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon ffmpeg-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon libtorrent
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mono
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon emby
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon ffmpeg-tools
