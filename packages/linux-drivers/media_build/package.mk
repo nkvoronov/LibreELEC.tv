72cb9a4699935a69a2a6d13e456cc44917f4ac16
Nikolay Voronov <nkvoronov@gmail.com>
2018-05-29 02:49:49 +0300

    update media_build

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="media_build"
-PKG_VERSION="4b106ea"
+PKG_VERSION="4a77bc4"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/crazycat69/linux_media"
