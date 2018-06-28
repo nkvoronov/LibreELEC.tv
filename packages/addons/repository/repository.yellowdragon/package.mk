432345fc2df7cea7a6065f5d28751041f8cce7f9
Nikolay Voronov <nkvoronov@gmail.com>
2018-06-28 17:11:43 +0300

    update1

--- packages/addons/repository/repository.yellowdragon/package.mk
+++ packages/addons/repository/repository.yellowdragon/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="repository.yellowdragon"
-PKG_VERSION="17.6"
+PKG_VERSION="17.7"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE=""
