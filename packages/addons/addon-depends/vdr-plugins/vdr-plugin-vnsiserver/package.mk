bf0f94bca77f3f8ebaa050d20b1ef11c42ec10ad
Nikolay Voronov <nkvoronov@gmail.com>
2018-07-26 07:17:37 +0300

    update2

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-vnsiserver/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="ab52d71"
+PKG_VERSION="3425b20"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/FernetMenta/vdr-plugin-vnsiserver"
