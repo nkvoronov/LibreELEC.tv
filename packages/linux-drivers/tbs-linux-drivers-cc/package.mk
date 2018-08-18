d6acc18917a0cd29bb731c0dc136b86db954a225
Nikolay Voronov <nkvoronov@gmail.com>
2018-08-18 13:49:20 +0300

    update6

--- packages/linux-drivers/tbs-linux-drivers-cc/package.mk
+++ packages/linux-drivers/tbs-linux-drivers-cc/package.mk
@@ -1,20 +1,6 @@
-################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tbs-linux-drivers-cc"
 PKG_VERSION="0d45df2"
@@ -30,8 +16,8 @@ PKG_PRIORITY="optional"
 PKG_SECTION="driver"
 PKG_SHORTDESC="Linux TBS tuner drivers"
 PKG_LONGDESC="Linux TBS tuner drivers"
-PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_IS_KERNEL_PKG="yes"
+PKG_TOOLCHAIN="manual"
 
 make_target() {
   cd $BUILD/$PKG_NAME-$PKG_VERSION
