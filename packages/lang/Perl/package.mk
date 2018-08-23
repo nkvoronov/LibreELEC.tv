f505e8fbc6eba67dde7b4e9da4c7fd7d0a5d5c6e
Nikolay Voronov <nkvoronov@gmail.com>
2018-08-23 21:51:48 +0300

    upd7

--- packages/lang/Perl/package.mk
+++ packages/lang/Perl/package.mk
@@ -2,16 +2,18 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="Perl"
-PKG_VERSION="5.24.1"
-PKG_SHA256="e6c185c9b09bdb3f1b13f678999050c639859a7ef39c8cad418448075f5918af"
+PKG_VERSION="5.28.0"
+PKG_SHA256="7e929f64d4cb0e9d1159d4a59fc89394e27fa1f7004d0836ca0d514685406ea8"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.cpan.org/"
-PKG_URL="http://www.cpan.org/src/5.0/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="http://www.cpan.org/src/5.0/perl-$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="perl-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="lang"
 PKG_SHORTDESC="Perl is a high-level programming language"
 PKG_LONGDESC="Perl is a high-level programming language"
+PKG_TOOLCHAIN="manual"
 
 pre_build_target() {
   rm -rf $PKG_BUILD/.$TARGET_NAME
