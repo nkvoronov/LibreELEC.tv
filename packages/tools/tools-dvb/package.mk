eac6bcc7154079f941f7e5c2b701f362d14dbfb4
Nikolay Voronov <nkvoronov@gmail.com>
2018-09-02 16:28:19 +0300

    init6

---
+++ packages/tools/tools-dvb/package.mk
@@ -0,0 +1,101 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+
+PKG_NAME="tools-dvb"
+PKG_VERSION="1.0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_SHORTDESC="A bundle of dvb tools and programs"
+PKG_LONGDESC="This bundle currently includes blindscan-s2, dvb-apps, dvblast, dvbsnoop, mumudvb, szap-s2, tune-s2 and w_scan."
+PKG_TOOLCHAIN="manual"
+
+ENABLE_BLINDSCAN_S2="no"
+ENABLE_DVB_APPS="no"
+ENABLE_DVBLAST="no"
+ENABLE_DVBSNOOP="no"
+ENABLE_MUMUDVB="no"
+ENABLE_SZAP_S2="yes"
+ENABLE_TUNE_S2="yes"
+ENABLE_WSCAN="yes"
+
+if [ "$ENABLE_BLINDSCAN_S2" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET blindscan-s2"
+fi
+
+if [ "$ENABLE_DVB_APPS" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dvb-apps"
+fi
+
+if [ "$ENABLE_DVBLAST" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dvblast"
+fi
+
+if [ "$ENABLE_DVBSNOOP" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dvbsnoop"
+fi
+
+if [ "$ENABLE_MUMUDVB" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mumudvb"
+fi
+
+if [ "$ENABLE_SZAP_S2" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET szap-s2"
+fi
+
+if [ "$ENABLE_TUNE_S2" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET tune-s2"
+fi
+
+if [ "$ENABLE_WSCAN" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET w_scan"
+fi
+
+post_install() {
+  mkdir -p $INSTALL/usr/bin/
+  # blindscan-s2
+  if [ "$ENABLE_BLINDSCAN_S2" = "yes" ]; then
+    cp -P $(get_build_dir blindscan-s2)/blindscan-s2 $INSTALL/usr/bin
+  fi
+  # dvb-apps
+  if [ "$ENABLE_DVB_APPS" = "yes" ]; then
+    cp -P $(get_build_dir dvb-apps)/util/dvbdate/dvbdate $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/dvbnet/dvbnet $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/dvbscan/dvbscan $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/dvbtraffic/dvbtraffic $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/femon/femon $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/scan/scan $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/szap/azap $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/szap/czap $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/szap/szap $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/szap/tzap $INSTALL/usr/bin
+    cp -P $(get_build_dir dvb-apps)/util/zap/zap $INSTALL/usr/bin
+  fi
+  # dvblast
+  if [ "$ENABLE_DVBLAST" = "yes" ]; then
+    cp -P $(get_build_dir dvblast)/dvblast $INSTALL/usr/bin
+  fi
+  # dvbsnoop
+  if [ "$ENABLE_DVBSNOOP" = "yes" ]; then
+    cp -P $(get_build_dir dvbsnoop)/.$TARGET_NAME/src/dvbsnoop $INSTALL/usr/bin
+  fi
+  # mumudvb
+  if [ "$ENABLE_MUMUDVB" = "yes" ]; then
+    cp -P $(get_build_dir mumudvb)/.$TARGET_NAME/src/mumudvb $INSTALL/usr/bin
+  fi
+  # szap-s2
+  if [ "$ENABLE_SZAP_S2" = "yes" ]; then
+    cp -P $(get_build_dir szap-s2)/szap-s2 $INSTALL/usr/bin
+  fi
+  # tune-s2
+  if [ "$ENABLE_TUNE_S2" = "yes" ]; then
+    cp -P $(get_build_dir tune-s2)/tune-s2 $INSTALL/usr/bin
+  fi
+  # w_scan
+  if [ "$ENABLE_WSCAN" = "yes" ]; then
+    cp -P $(get_build_dir w_scan)/.$TARGET_NAME/w_scan $INSTALL/usr/bin
+  fi
+}
---
+++ packages/tools/tools-system/package.mk
@@ -0,0 +1,346 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+
+PKG_NAME="tools-system"
+PKG_VERSION="1.0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_SHORTDESC="A bundle of system tools and programs"
+PKG_LONGDESC="This bundle currently includes autossh, diffutils, dstat, dtach, efibootmgr, encfs, evtest, fdupes, file, getscancodes, hddtemp, hd-idle, hid_mapper, htop, i2c-tools, inotify-tools, jq, lm_sensors, lshw, mc, mrxvt, mtpfs, nmon, p7zip, patch, pv, screen, smartmontools, strace, stress-ng, unrar, usb-modeswitch and vim."
+PKG_TOOLCHAIN="manual"
+
+ENABLE_AUTOSSH="no"
+ENABLE_DIFFUTILS="no"
+ENABLE_DSTAT="no"
+ENABLE_DTACH="no"
+ENABLE_EFIBOOTMGR="no"
+ENABLE_ENCFS="no"
+ENABLE_EVTEST="yes"
+ENABLE_FDUPES="no"
+ENABLE_FILE="no"
+ENABLE_GETSCANCODES="no"
+ENABLE_HDDTEMP="yes"
+ENABLE_HD_IDLE="no"
+ENABLE_HID_MAPPER="no"
+ENABLE_HTOP="yes"
+ENABLE_I2C_TOOLS="no"
+ENABLE_INOTIFY_TOOLS="no"
+ENABLE_JQ="no"
+ENABLE_LM_SENSORS="yes"
+ENABLE_LSHW="no"
+ENABLE_MC="yes"
+ENABLE_MRXVT="no"
+ENABLE_MTPFS="no"
+ENABLE_NMON="no"
+ENABLE_P7ZIP="yes"
+ENABLE_PATCH="no"
+ENABLE_PV="no"
+ENABLE_SCREEN="no"
+ENABLE_SMARTMONTOOLS="no"
+ENABLE_STRACE="no"
+ENABLE_STRACE_NG="no"
+ENABLE_UNRAR="yes"
+ENABLE_USB_MODESWITCH="no"
+ENABLE_VIM="no"
+
+if [ "$ENABLE_AUTOSSH" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET autossh"
+fi
+
+if [ "$ENABLE_DIFFUTILS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET diffutils"
+fi
+
+if [ "$ENABLE_DSTAT" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dstat"
+fi
+
+if [ "$ENABLE_DTACH" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dtach"
+fi
+
+if [ "$ENABLE_EFIBOOTMGR" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET efibootmgr"
+fi
+
+if [ "$ENABLE_ENCFS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET encfs"
+fi
+
+if [ "$ENABLE_EVTEST" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET evtest"
+fi
+
+if [ "$ENABLE_FDUPES" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET fdupes"
+fi
+
+if [ "$ENABLE_FILE" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET file"
+fi
+
+if [ "$ENABLE_GETSCANCODES" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET getscancodes"
+fi
+
+if [ "$ENABLE_HDDTEMP" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET hddtemp"
+fi
+
+if [ "$ENABLE_HD_IDLE" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET hd-idle"
+fi
+
+if [ "$ENABLE_HID_MAPPER" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET hid_mapper"
+fi
+
+if [ "$ENABLE_HTOP" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET htop"
+fi
+
+if [ "$ENABLE_I2C_TOOLS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET i2c-tools"
+fi
+
+if [ "$ENABLE_INOTIFY_TOOLS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET inotify-tools"
+fi
+
+if [ "$ENABLE_JQ" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET jq"
+fi
+
+if [ "$ENABLE_LM_SENSORS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET lm_sensors"
+fi
+
+if [ "$ENABLE_LSHW" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET lshw"
+fi
+
+if [ "$ENABLE_MC" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mc"
+fi
+
+if [ "$ENABLE_MRXVT" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mrxvt"
+fi
+
+if [ "$ENABLE_MTPFS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET mtpfs"
+fi
+
+if [ "$ENABLE_NMON" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET nmon"
+fi
+
+if [ "$ENABLE_P7ZIP" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET p7zip"
+fi
+
+if [ "$ENABLE_PATCH" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET patch"
+fi
+
+if [ "$ENABLE_PV" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET pv"
+fi
+
+if [ "$ENABLE_SCREEN" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET screen"
+fi
+
+if [ "$ENABLE_SMARTMONTOOLS" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET smartmontools"
+fi
+
+if [ "$ENABLE_STRACE" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET strace"
+fi
+
+if [ "$ENABLE_STRACE_NG" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET stress-ng"
+fi
+
+if [ "$ENABLE_UNRAR" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET unrar"
+fi
+
+if [ "$ENABLE_USB-MODESWITCH" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET usb-modeswitch"
+fi
+
+if [ "$ENABLE_VIM" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET vim"
+fi
+
+post_install() {
+  mkdir -p $INSTALL/
+  mkdir -p $INSTALL/usr/bin/
+  mkdir -p $INSTALL/usr/lib/
+  # autossh
+  if [ "$ENABLE_AUTOSSH" = "yes" ]; then
+    cp -P $(get_build_dir autossh)/.$TARGET_NAME/autossh $INSTALL/usr/bin
+  fi
+  # diffutils
+  if [ "$ENABLE_DIFFUTILS" = "yes" ]; then
+    cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/cmp $INSTALL/usr/bin
+    cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/diff $INSTALL/usr/bin
+    cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/diff3 $INSTALL/usr/bin
+    cp -P $(get_build_dir diffutils)/.$TARGET_NAME/src/sdiff $INSTALL/usr/bin
+  fi
+  # dstat
+  if [ "$ENABLE_DSTAT" = "yes" ]; then
+    cp -P $(get_build_dir dstat)/dstat $INSTALL/usr/bin
+  fi
+  # dtach
+  if [ "$ENABLE_DTACH" = "yes" ]; then
+    cp -P $(get_build_dir dtach)/.$TARGET_NAME/dtach $INSTALL/usr/bin
+  fi
+  # efibootmgr
+  if [ "$ENABLE_EFIBOOTMGR" = "yes" ]; then
+    cp -P $(get_build_dir efibootmgr)/src/efibootmgr/efibootmgr $INSTALL/usr/bin 2>/dev/null || :
+  fi
+  # encfs
+  if [ "$ENABLE_ENCFS" = "yes" ]; then
+    cp -P $(get_build_dir encfs)/.$TARGET_NAME/encfs $INSTALL/usr/bin
+    cp -P $(get_build_dir encfs)/.$TARGET_NAME/encfsctl $INSTALL/usr/bin
+  fi
+  # evtest
+  if [ "$ENABLE_EVTEST" = "yes" ]; then
+    cp -P $(get_build_dir evtest)/.$TARGET_NAME/evtest $INSTALL/usr/bin
+  fi
+  # fdupes
+  if [ "$ENABLE_FDUPES" = "yes" ]; then
+    cp -P $(get_build_dir fdupes)/fdupes $INSTALL/usr/bin
+  fi
+  # file
+  if [ "$ENABLE_FILE" = "yes" ]; then
+    cp -P $(get_build_dir file)/.$TARGET_NAME/src/file $INSTALL/usr/bin
+    mkdir -p $INSTALL/usr/data/
+    cp -P $(get_build_dir file)/.$TARGET_NAME/magic/magic.mgc $INSTALL/usr/data
+  fi
+  # getscancodes
+  if [ "$ENABLE_GETSCANCODES" = "yes" ]; then
+    cp -P $(get_build_dir getscancodes)/getscancodes $INSTALL/usr/bin
+  fi
+  # hddtemp
+  if [ "$ENABLE_HDDTEMP" = "yes" ]; then
+    cp -P $(get_build_dir hddtemp)/.$TARGET_NAME/src/hddtemp $INSTALL/usr/bin
+    mkdir -p $INSTALL/etc
+    cp -P $(get_build_dir hddtemp)/hddtemp.db $INSTALL/etc
+  fi
+  # hd-idle
+  if [ "$ENABLE_HD_IDLE" = "yes" ]; then
+    cp -P $(get_build_dir hd-idle)/hd-idle $INSTALL/usr/bin
+  fi
+  # hid_mapper
+  if [ "$ENABLE_HID_MAPPER" = "yes" ]; then
+    cp -P $(get_build_dir hid_mapper)/hid_mapper $INSTALL/usr/bin
+  fi
+  # htop
+  if [ "$ENABLE_HTOP" = "yes" ]; then
+    cp -P $(get_build_dir htop)/.install_pkg/usr/bin/htop $INSTALL/usr/bin
+  fi
+  # i2c-tools
+  if [ "$ENABLE_I2C_TOOLS" = "yes" ]; then
+    cp -P $(get_build_dir i2c-tools)/tools/i2cdetect $INSTALL/usr/bin
+    cp -P $(get_build_dir i2c-tools)/tools/i2cdump $INSTALL/usr/bin
+    cp -P $(get_build_dir i2c-tools)/tools/i2cget $INSTALL/usr/bin
+    cp -P $(get_build_dir i2c-tools)/tools/i2cset $INSTALL/usr/bin
+    cp -P $(get_build_dir i2c-tools)/py-smbus/build/lib.linux-*/smbus.so $INSTALL/usr/lib
+  fi
+  # inotify-tools
+  if [ "$ENABLE_INOTIFY_TOOLS" = "yes" ]; then
+    cp -P $(get_build_dir inotify-tools)/.$TARGET_NAME/src/inotifywait $INSTALL/usr/bin
+    cp -P $(get_build_dir inotify-tools)/.$TARGET_NAME/src/inotifywatch $INSTALL/usr/bin
+  fi
+  # jq
+  if [ "$ENABLE_JQ" = "yes" ]; then
+    cp -P $(get_build_dir jq)/.$TARGET_NAME/jq $INSTALL/usr/bin
+  fi
+  # lm_sensors
+  if [ "$ENABLE_LM_SENSORS" = "yes" ]; then
+    cp -P $(get_build_dir lm_sensors)/prog/sensors/sensors $INSTALL/usr/bin 2>/dev/null || :
+  fi
+  # lshw
+  if [ "$ENABLE_LSHW" = "yes" ]; then
+    cp -P $(get_build_dir lshw)/src/lshw $INSTALL/usr/bin
+  fi
+  # mc
+  if [ "$ENABLE_MC" = "yes" ]; then
+    cp -PR  $(get_build_dir mc)/.install_pkg/* $INSTALL
+    mkdir -p $INSTALL/usr/share/mc/bin
+    ln -s /usr/libexec/mc/mc-wrapper.csh $INSTALL/usr/share/mc/bin/mc-wrapper.csh
+    ln -s /usr/libexec/mc/mc-wrapper.sh $INSTALL/usr/share/mc/bin/mc-wrapper.sh
+    ln -s /usr/libexec/mc/mc.csh $INSTALL/usr/share/mc/bin/mc.csh
+    ln -s /usr/libexec/mc/mc.sh $INSTALL/usr/share/mc/bin/mc.sh
+    mkdir -p $INSTALL/usr/share/locale
+    for fgmo in `ls $(get_build_dir mc)/po/*.gmo`;do
+      fname=`basename $fgmo .gmo`
+      mkdir -p $INSTALL/usr/share/locale/$fname
+      mkdir -p $INSTALL/usr/share/locale/$fname/LC_MESSAGES
+      cp -p $fgmo $INSTALL/usr/share/locale/$fname/LC_MESSAGES/mc.mo
+    done
+  fi
+  # mrxvt
+  if [ "$ENABLE_MRXVT" = "yes" ]; then
+    cp -P $(get_build_dir mrxvt)/.$TARGET_NAME/src/mrxvt $INSTALL/usr/bin 2>/dev/null || :
+  fi
+  # mtpfs
+  if [ "$ENABLE_MTPFS" = "yes" ]; then
+    cp -P $(get_build_dir mtpfs)/.$TARGET_NAME/mtpfs $INSTALL/usr/bin/
+  fi
+  # nmon
+  if [ "$ENABLE_NMON" = "yes" ]; then
+    cp -P $(get_build_dir nmon)/nmon $INSTALL/usr/bin/
+  fi
+  # p7zip
+  if [ "$ENABLE_P7ZIP" = "yes" ]; then
+    cp -P $(get_build_dir p7zip)/bin/7z.so $INSTALL/usr/bin
+    cp -PR $(get_build_dir p7zip)/bin/Codecs $INSTALL/usr/bin
+    cp -P $(get_build_dir p7zip)/bin/7z $INSTALL/usr/bin
+    cp -P $(get_build_dir p7zip)/bin/7za $INSTALL/usr/bin
+  fi
+  # patch
+  if [ "$ENABLE_PATCH" = "yes" ]; then
+    cp -P $(get_build_dir patch)/.$TARGET_NAME/src/patch $INSTALL/usr/bin
+  fi
+  # pv
+  if [ "$ENABLE_PV" = "yes" ]; then
+    cp -P $(get_build_dir pv)/.$TARGET_NAME/pv $INSTALL/usr/bin
+  fi
+  # screen
+  if [ "$ENABLE_SCREEN" = "yes" ]; then
+    cp -P $(get_build_dir screen)/screen $INSTALL/usr/bin
+  fi
+  # smartmontools
+  if [ "$ENABLE_SMARTMONTOOLS" = "yes" ]; then
+    cp -P $(get_build_dir smartmontools)/.$TARGET_NAME/smartctl $INSTALL/usr/bin
+  fi
+  # strace
+  if [ "$ENABLE_STRACE" = "yes" ]; then
+    cp -P $(get_build_dir strace)/.$TARGET_NAME/strace $INSTALL/usr/bin
+  fi
+  # strace-ng
+  if [ "$ENABLE_STRACE_NG" = "yes" ]; then
+    cp -P $(get_build_dir stress-ng)/.install_pkg/usr/bin/stress-ng $INSTALL/usr/bin
+  fi
+  # unrar
+  if [ "$ENABLE_UNRAR" = "yes" ]; then
+    cp -P $(get_build_dir unrar)/unrar $INSTALL/usr/bin
+  fi
+  # usb-modeswitch
+  if [ "$ENABLE_USB_MODESWITCH" = "yes" ]; then
+    cp -P $(get_build_dir usb-modeswitch)/usb_modeswitch $INSTALL/usr/bin
+  fi
+  # vim
+  if [ "$ENABLE_VIM" = "yes" ]; then
+    cp -P $(get_build_dir vim)/.install_pkg/usr/bin/vim $INSTALL/usr/bin
+  fi
+}
