3052d7617f28d75216e7823cb7fb0539029a219f
Nikolay Voronov <nkvoronov@gmail.com>
2019-01-22 06:50:21 +0200

    update

--- packages/mediacenter/kodi-binary-addons/pvr.hdhomerun/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.hdhomerun/package.mk
@@ -2,18 +2,18 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
-PKG_NAME="pvr.hts"
-PKG_VERSION="a7f101b7274948f4aea307fee2b30b18a7dce473"
-PKG_SHA256="3bb9289cb5bf2781164c65d776ee1c2cb46997a506864ac6bc1f87b38bb55fc3"
+PKG_NAME="pvr.hdhomerun"
+PKG_VERSION="a9d730977ec2d529262cd19d81f0230e1def136b"
+PKG_SHA256="21f872965fc81aa87213c0c182e63b7cd2058d9213eb92f0cd1e3310cfee690b"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
-PKG_URL="https://github.com/kodi-pvr/pvr.hts/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_URL="https://github.com/kodi-pvr/pvr.hdhomerun/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform jsoncpp libhdhomerun"
 PKG_SECTION=""
-PKG_SHORTDESC="pvr.hts"
-PKG_LONGDESC="pvr.hts"
+PKG_SHORTDESC="pvr.hdhomerun"
+PKG_LONGDESC="pvr.hdhomerun"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.pvrclient"
