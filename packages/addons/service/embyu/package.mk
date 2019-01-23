4f9bde6fbbb6b5ebf191bd5aed3ccac419e7a129
Nikolay Voronov <nkvoronov@gmail.com>
2019-01-23 22:39:22 +0200

    update emby

--- packages/addons/service/embyu/package.mk
+++ packages/addons/service/embyu/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="embyu"
-PKG_VERSION="4.0.0.2"
-PKG_SHA256="1a50e4499ff8037a96aa9748719e65fd3fe6c8299f17cd74aa29e50613b488b7"
-PKG_REV="358"
+PKG_VERSION="4.0.1.0"
+#PKG_SHA256="1a50e4499ff8037a96aa9748719e65fd3fe6c8299f17cd74aa29e50613b488b7"
+PKG_REV="359"
 PKG_LICENSE="OSS"
 PKG_SITE="http://emby.media"
 PKG_URL="https://github.com/MediaBrowser/Emby.Releases/releases/download/${PKG_VERSION}/emby-server-deb_${PKG_VERSION}_amd64.deb"
