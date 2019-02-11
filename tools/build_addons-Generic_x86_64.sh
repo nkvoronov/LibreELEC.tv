af1b50afd2c1568f3628918cdcde9bbbe6d305c1
Nikolay Voronov <nkvoronov@gmail.com>
2019-02-11 23:11:50 +0200

    fixes

--- tools/build_addons-Generic_x86_64.sh
+++ tools/build_addons-Generic_x86_64.sh
@@ -2,18 +2,18 @@
 
 cd ..
 
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr24
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-engine
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon docker
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon embyu
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr24
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-engine
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon docker
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon embyu
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc3
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon poppler
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon poppler
 
 
 
