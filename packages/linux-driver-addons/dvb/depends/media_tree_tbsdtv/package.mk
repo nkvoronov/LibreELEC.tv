6ceeea07c89480afae1ebcddeccae216662177bb
Nikolay Voronov <nkvoronov@gmail.com>
2019-03-13 16:48:45 +0200

    update drivers

--- packages/linux-driver-addons/dvb/depends/media_tree_tbsdtv/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_tbsdtv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="media_tree_tbsdtv"
-PKG_VERSION="f539dd63c402b8354a37f4c164f5ddd4d589bf9c"
-PKG_SHA256="ebc0808bf50107a381c3472a553b153c5428d8e31829f7c07e5b3c5f30113b9d"
+PKG_VERSION="5d9c46813f2030767e595e42865e2759c0e5e11a"
+PKG_SHA256="862b8a6e390383e3c97285e387aab91d027b82f78bb288af5edd929295e7b98b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/tbsdtv/linux_media.git"
 PKG_URL="https://github.com/tbsdtv/linux_media/archive/$PKG_VERSION.tar.gz"
