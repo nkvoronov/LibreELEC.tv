d81bb0111f3904642b5390cb833230ad01c57bf7
Nikolay Voronov <nkvoronov@gmail.com>
2019-08-08 04:58:51 +0300

    update

--- packages/addons/addon-depends/libevent/package.mk
+++ packages/addons/addon-depends/libevent/package.mk
@@ -1,17 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libevent"
-PKG_VERSION="2.0.22-stable"
-PKG_SHA256="71c2c49f0adadacfdbe6332a372c38cf9c8b7895bb73dabeaa53cdcc1d4e1fa3"
+PKG_VERSION="2.1.11"
+PKG_SHA256="a65bac6202ea8c5609fd5c7e480e6d25de467ea1917c08290c521752f147283d"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
-PKG_URL="https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION-stable/$PKG_NAME-$PKG_VERSION-stable.tar.gz"
 PKG_DEPENDS_TARGET="toolchain openssl zlib"
 PKG_LONGDESC="The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached. It is meant to replace the asynchronous event loop found in event-driven network servers."
 PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --enable-openssl"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --disable-libevent-regress"
 
 post_makeinstall_target() {
   rm -r $INSTALL
--- packages/addons/addon-depends/vdr-depends/ImageMagick/package.mk
+++ packages/addons/addon-depends/vdr-depends/ImageMagick/package.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2009-2019 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="ImageMagick"
-PKG_VERSION="6.9.10-33"
+PKG_VERSION="6.9.10-59"
 #PKG_VERSION="6.9.10-12"
-#PKG_SHA256="bb0224b9a530dc89b792594c5d1f04e568a976b456fd6c738715d2cf80ab8409"
+PKG_SHA256="63a4fe17c56902fb7bc9d6381f2b53c8087ce979a070688ad4d61e68a7343b60"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.imagemagick.org"
 PKG_URL="http://www.imagemagick.org/download/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -23,8 +23,7 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-shared \
                            --without-lcms \
                            --without-tiff \
                            --without-xml \
-                           --without-x \
-                           --without-utilities"
+                           --without-x"
 
 makeinstall_target() {
   make install DESTDIR=$INSTALL $PKG_MAKEINSTALL_OPTS_TARGET
