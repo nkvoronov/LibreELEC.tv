e8cebd95aa7771753f5c3061afbb320002bd62ee
Nikolay Voronov <nkvoronov@gmail.com>
2019-09-10 02:32:33 +0300

    update3

--- tools/build_addons-Generic_x86_64.sh
+++ tools/build_addons-Generic_x86_64.sh
@@ -2,19 +2,19 @@
 
 cd ..
 
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr24
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-engine
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon docker
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon embyu
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mariadb
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon poppler
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc3
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission2
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vdr24
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon acestream-engine
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon docker
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon embyu
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon mariadb
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon poppler
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon vlc3
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon jellyfin
 
 
