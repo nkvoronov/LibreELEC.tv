59baa28ab26e90debec4ac84d04d03768ed4f2ca
Nikolay Voronov <nkvoronov@gmail.com>
2020-09-05 19:46:10 +0000

    update

--- packages/addons/tools/aspnet-runtime3/package.mk
+++ packages/addons/tools/aspnet-runtime3/package.mk
@@ -4,24 +4,24 @@
 case "$ARCH" in
   "aarch64")
     PKG_NC_ARCH="arm64"
-    PKG_SHA256="9dab4863b0203bf7b6b59f3533e55f7dda3624fe209eef3199510e917df9da13"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/d5c6e9d7-25b9-47ac-9d67-35ac65211ad3/c8f4ccd0dc02ca8229ba43ecbe84294b/aspnetcore-runtime-3.1.3-linux-arm64.tar.gz"
+    #PKG_SHA256="d5e97a7c3e495ffe03f62415802a5a6f8fc57e9ee86f7730e6752b016747514507fbf79c0e65a815b9d09fb35af05026ca8ea99626535b31703a217559909309"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/e0982947-c956-4c44-b94a-3ecc13d7aa64/28f9a7f461d5aac85121492ba4513517/dotnet-runtime-3.1.7-linux-arm64.tar.gz"
     ;;
   "arm")
     PKG_NC_ARCH="arm"
-    PKG_SHA256="467490cb72058017e557aa8c20c9c5f45d6f3f23d75c530cedb819dea2f7270d"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/b68cde83-05c7-4421-ad9a-3e6f2cc53824/876dbfc9b4521d3ca89a226c6438ffc1/aspnetcore-runtime-3.1.3-linux-arm.tar.gz"
+    #PKG_SHA256="a586ed88cebbef298cda04c94e1809017800abea50668e18bcf27c3aa1ca114bb16963bcdcc4fd535288838120cba52966d594ef395bb8fac11693f64c97ccc1"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/69984653-402e-442c-9588-eb92560d0fce/5ad7995a09334dd2ee56f00fb6dc0521/dotnet-runtime-3.1.7-linux-arm.tar.gz"
     ;;
   "x86_64")
     PKG_NC_ARCH="x64"
-    PKG_SHA256="34cc5c7f16b45234603d9f352a1bd3997d5e07d7380c49266860f75dbc29146d"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/7faca87b-7526-4dcd-ae23-4559d2c51ce3/7db1f314c733191ea43e1757e3b2583d/aspnetcore-runtime-3.1.3-linux-x64.tar.gz"
+    #PKG_SHA256="4f0ce619c1b1dbc8ccd799877b5d73158a07b1ebd1222d44b909bba13bdf735c"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/e7d0601d-41b4-483f-b411-f2b42708054a/191b56b81e1830b413d0794728831eea/aspnetcore-runtime-3.1.7-linux-x64.tar.gz"
     ;;
 esac
 
 PKG_NAME="aspnet-runtime3"
-PKG_VERSION="3.1.3"
-PKG_REV="111"
+PKG_VERSION="3.1.7"
+PKG_REV="112"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.github.io/"
