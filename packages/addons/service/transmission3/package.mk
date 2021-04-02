6b26c4c66a2de913a8a2105746e211db39640279
Nikolay Voronov <nkvoronov@gmail.com>
2021-04-02 10:41:01 +0300

    update

---
+++ packages/addons/service/transmission3/package.mk
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="transmission3"
+PKG_VERSION="3.00"
+PKG_REV="11"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.transmissionbt.com/"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain transmission"
+PKG_SECTION="service"
+PKG_SHORTDESC="transmission: a fast, easy and free BitTorrent client"
+PKG_LONGDESC="transmission is a fast, easy and free BitTorrent client"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Transmission"
+PKG_ADDON_TYPE="xbmc.service"
+
+addon() {
+  TRANSMISSION_DIR=$(get_build_dir transmission)
+
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp ${TRANSMISSION_DIR}/.${TARGET_NAME}/daemon/transmission-daemon ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp ${TRANSMISSION_DIR}/.${TARGET_NAME}/utils/transmission-remote ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp ${TRANSMISSION_DIR}/.${TARGET_NAME}/utils/transmission-create ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp ${TRANSMISSION_DIR}/.${TARGET_NAME}/utils/transmission-edit ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp ${TRANSMISSION_DIR}/.${TARGET_NAME}/utils/transmission-show ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/web
+    cp -R ${TRANSMISSION_DIR}/web/* ${ADDON_BUILD}/${PKG_ADDON_ID}/web
+    find ${ADDON_BUILD}/${PKG_ADDON_ID}/web -name "Makefile*" -exec rm -rf {} ";"
+    rm -rf ${ADDON_BUILD}/${PKG_ADDON_ID}/web/LICENSE
+}
