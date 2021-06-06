41a78491d010161debc04d12fbe713918a00909a
Nikolay Voronov <nkvoronov@gmail.com>
2021-06-06 16:30:39 +0300

    update

--- packages/addons/service/tvheadend43/package.mk
+++ packages/addons/service/tvheadend43/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tvheadend43"
-PKG_VERSION="4.3.1964"
-PKG_REV="292"
+PKG_VERSION="4.3.1965"
+PKG_REV="293"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
