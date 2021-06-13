4c017e41d4cce6c67292eac08a103be697b3bfdf
Nikolay Voronov <nkvoronov@gmail.com>
2021-06-13 19:06:11 +0300

    update

--- packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mpg123"
-PKG_VERSION="1.26.5"
-PKG_SHA256="502a97e0d935be7e37d987338021d8f301bae35c2884f2a83d59c4b52466ef06"
+PKG_VERSION="1.27.2"
+PKG_SHA256="52f6ceb962c05db0c043bb27acf5a721381f5f356ac4610e5221f50293891b04"
 PKG_LICENSE="LGPLv2"
 PKG_SITE="http://www.mpg123.org/"
 PKG_URL="http://downloads.sourceforge.net/sourceforge/mpg123/mpg123-${PKG_VERSION}.tar.bz2"
