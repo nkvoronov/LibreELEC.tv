b50a7bb1b659e96a800c4326223c732d72313d92
Nikolay Voronov <nkvoronov@gmail.com>
2021-07-24 13:45:27 +0300

    update

--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="7.6.7-Matrix"
-PKG_SHA256="86ea2dba6a99dec2b1968e1e73139b7d96cb0dd7c0ffd01bda414a6be0cf3050"
+PKG_VERSION="7.6.9-Matrix"
+PKG_SHA256="b968b38d09299a9784b82d3fb82f461db1544901b450e374d270dd5c2be0c6d1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
