099bcb5d4116001b3d0945739c68c64ff3426dbe
Nikolay Voronov <nkvoronov@gmail.com>
2021-11-01 20:27:40 +0200

    update

--- packages/linux-driver-addons/dvb/depends/media_tree_tbsdtv/package.mk
+++ packages/linux-driver-addons/dvb/depends/media_tree_tbsdtv/package.mk
@@ -2,22 +2,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="media_tree_tbsdtv"
-PKG_VERSION="50b5a6dfd1b7fc9abf8e629de05208c3da44b97d"
-#PKG_SHA256="2084cd49b72d3acf6406c79ddb231c5e3945f532e3a74c1d62a6ca9fe4406a89"
+PKG_VERSION="50b5a6d"
+PKG_GIT_CLONE_BRANCH="latest"
 PKG_SITE="https://github.com/tbsdtv/linux_media.git"
-PKG_URL="https://github.com/tbsdtv/linux_media/archive/${PKG_VERSION}.tar.gz"
-PKG_SOURCE_DIR="linux_media-${PKG_VERSION}"
+PKG_URL="https://github.com/tbsdtv/linux_media.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_LONGDESC="Source of Linux Kernel media_tree subsystem to build with media_build."
 PKG_TOOLCHAIN="manual"
-
-post_unpack() {
-  # hack/workaround for borked upstream kernel/media_build
-  # without removing atomisp there a lot additional includes that 
-  # slowdown build process after modpost from 3min to 6min
-  # even if atomisp is disabled via kernel.conf
-  rm -rf ${PKG_BUILD}/drivers/staging/media/atomisp
-  sed -i 's|^.*drivers/staging/media/atomisp.*$||' \
-    ${PKG_BUILD}/drivers/staging/media/Kconfig
-}
--- packages/linux-driver-addons/dvb/tbsdtv/package.mk
+++ packages/linux-driver-addons/dvb/tbsdtv/package.mk
@@ -3,15 +3,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tbsdtv"
-PKG_VERSION="20210725"
-PKG_SHA256="972c3edb6be67151475360272c825cc8d2ec705d4d6b18519fdff3731579e728"
-#PKG_VERSION="945eea79087069a56c4989288c935a2602b3fd92"
-#PKG_SHA256="d0b03eca4293254e0f86568f2ae62bb59146d2323f202d5959f3f2f2cfb8850d"
+PKG_VERSION="945eea7"
+PKG_GIT_CLONE_BRANCH="extra"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/tbsdtv/media_build.git"
-PKG_URL="https://www.dropbox.com/s/4qhr1zxs9owg3pd/tbsdtv-${PKG_VERSION}.tar.gz"
-#PKG_URL="https://github.com/tbsdtv/media_build/archive/${PKG_VERSION}.tar.gz"
-#PKG_DEPENDS_UNPACK="media_tree_tbsdtv"
+PKG_URL="https://github.com/tbsdtv/media_build.git"
+PKG_DEPENDS_UNPACK="media_tree_tbsdtv"
 PKG_SECTION="driver.dvb"
 PKG_LONGDESC="TBSDTV linux open source drivers"
 PKG_TOOLCHAIN="manual"
@@ -25,44 +22,33 @@ PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
 
 PKG_KERNEL_CFG_FILE=$(kernel_config_path) || die
 
-#if ! grep -q ^CONFIG_USB_PCI= ${PKG_KERNEL_CFG_FILE}; then
-#  PKG_PATCH_DIRS="disable-pci"
-#fi
+if ! grep -q ^CONFIG_USB_PCI= ${PKG_KERNEL_CFG_FILE}; then
+  PKG_PATCH_DIRS="disable-pci"
+fi
 
-#pre_make_target() {
-#  export KERNEL_VER=$(get_module_dir)
-#  export LDFLAGS=""
-#}
-
-#make_target() {
-#  mkdir -p ${PKG_BUILD}/media
-#  cp -RP $(get_build_dir media_tree_tbsdtv)/* ${PKG_BUILD}/media
-#  make dir DIR=./media
-#
-#  #cp -RP $(get_build_dir media_tree_tbsdtv)/* ${PKG_BUILD}/linux
-#
-#  # make config all
-#  kernel_make VER=${KERNEL_VER} SRCDIR=$(kernel_path) allyesconfig
-#
-#  # hack to workaround media_build bug
-#  if [ "${PROJECT}" = Rockchip ]; then
-#    sed -e 's/CONFIG_DVB_CXD2820R=m/# CONFIG_DVB_CXD2820R is not set/g' -i v4l/.config
-#    sed -e 's/CONFIG_DVB_LGDT3306A=m/# CONFIG_DVB_LGDT3306A is not set/g' -i v4l/.config
-#  fi
-#
-#  # add menuconfig to edit .config
-#  export MAX_ARG_PAGES=64
-#  kernel_make -j4 VER=${KERNEL_VER} SRCDIR=$(kernel_path)
-#}
+pre_make_target() {
+  export KERNEL_VER=$(get_module_dir)
+  export LDFLAGS=""
+}
 
 make_target() {
-  :
+  mkdir -p ${PKG_BUILD}/media
+  cp -RP $(get_build_dir media_tree_tbsdtv)/* ${PKG_BUILD}/media
+  make dir DIR=./media
+
+  # make config all
+  kernel_make VER=${KERNEL_VER} SRCDIR=$(kernel_path) allyesconfig
+
+  # hack to workaround media_build bug
+  if [ "${PROJECT}" = Rockchip ]; then
+    sed -e 's/CONFIG_DVB_CXD2820R=m/# CONFIG_DVB_CXD2820R is not set/g' -i v4l/.config
+    sed -e 's/CONFIG_DVB_LGDT3306A=m/# CONFIG_DVB_LGDT3306A is not set/g' -i v4l/.config
+  fi
+
+  # add menuconfig to edit .config
+  kernel_make VER=${KERNEL_VER} SRCDIR=$(kernel_path)
 }
 
 makeinstall_target() {
-  install_driver_addon_files "${PKG_BUILD}/"
+  install_driver_addon_files "${PKG_BUILD}/v4l/"
 }
-
-#makeinstall_target() {
-#  install_driver_addon_files "${PKG_BUILD}/v4l/"
-#}
