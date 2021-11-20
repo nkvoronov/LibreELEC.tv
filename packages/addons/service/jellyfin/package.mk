72189659f54490d9ad61a2293b4e34ce057b30ec
Nikolay Voronov <nkvoronov@gmail.com>
2021-11-20 07:00:09 +0200

    update

--- packages/addons/service/jellyfin/package.mk
+++ packages/addons/service/jellyfin/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="jellyfin"
 PKG_VERSION="10.7.7"
 PKG_SHA256="41390511b6dca6d14aa5f3d8cb397a7af519c923a6125f85e5c76571a87dc816"
-PKG_REV="138"
+PKG_REV="139"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://jellyfin.github.io/"
