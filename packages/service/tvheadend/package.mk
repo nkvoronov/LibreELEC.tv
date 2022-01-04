d561b60ec862b6d2b88509d18bce76924d615ca8
Nikolay Voronov <nkvoronov@gmail.com>
2022-01-04 03:43:47 +0200

    update

--- packages/service/tvheadend/package.mk
+++ packages/service/tvheadend/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tvheadend"
-PKG_VERSION="09a2c71ab"
-PKG_VERSION_NUMBER="4.3.1986"
+PKG_VERSION="3ed76138a"
+PKG_VERSION_NUMBER="4.3.1991"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
 PKG_URL="https://github.com/tvheadend/tvheadend.git"
