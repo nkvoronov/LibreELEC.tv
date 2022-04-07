9faddca56714a40f86a5256494aa843c0aa494ba
Nikolay Voronov <nkvoronov@gmail.com>
2022-04-07 07:46:21 +0300

    update

--- packages/linux-driver-addons/dvb/tbsdtv/package.mk
+++ packages/linux-driver-addons/dvb/tbsdtv/package.mk
@@ -5,9 +5,9 @@
 PKG_NAME="tbsdtv"
 
 if [ "${TBSDTV_EXT}" = "yes" ]; then
-   PKG_VERSION="20220403"
-   PKG_SHA256="0b5aef7906556cb7a6b80b6897ca004b48d396d1a6f98f2563080a207cd10c66"
-   PKG_URL="https://www.dropbox.com/s/jk2ky7oc1kxgt5s/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+   PKG_VERSION="20220407"
+   PKG_SHA256="ad14156ab629c26baa77e5a7a34495a21e576087b4185059be19c210bf53a683"
+   PKG_URL="https://www.dropbox.com/s/dydwvzgwf6m11gi/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 else
    PKG_VERSION="945eea7"
    PKG_GIT_CLONE_BRANCH="extra"
