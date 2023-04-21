53b59d86060c80315053cf6931e4db00c58695ae
Nikolay Voronov <nkvoronov@gmail.com>
2023-04-21 20:28:31 +0000

    update

--- tools/build_addons-Generic_x86_64.sh
+++ tools/build_addons-Generic_x86_64.sh
@@ -2,15 +2,15 @@
 
 cd ..
 
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission4
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
-#PROJECT=Generic ARCH=x86_64 ./scripts/create_addon docker
-
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon oscam12
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon tvheadend43
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon transmission4
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon lcdd
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon system-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dvb-tools
+PROJECT=Generic ARCH=x86_64 ./scripts/create_addon docker
 PROJECT=Generic ARCH=x86_64 ./scripts/create_addon ffmpeg-tools
+
 #PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dotnet-runtime
 #PROJECT=Generic ARCH=x86_64 ./scripts/create_addon dotnet-runtime6
 #PROJECT=Generic ARCH=x86_64 ./scripts/create_addon jellyfin
